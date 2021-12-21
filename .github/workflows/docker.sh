9860783837660d2ba6236868e8a1ff287f9425ca
zhoujianpiao <zhoujianpiao@ghzhushou.com>
2021-12-21 12:01:16 +0800

    修改镜像命令

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -3,6 +3,9 @@ containerName="koa-docker"
 # 镜像名称
 imageName="smallzip7799/koa-github-action"
 
+# 拉取镜像
+docker pull "${imageName}"
+
 # 根据容器名称，检查容器是否存在
 docker inspect -f '{{.Name}}' $containerName
 if [ $? == 0 ] ;then
@@ -17,7 +20,5 @@ else
     echo "docker don't exist"
 fi
 
-docker pull smallzip7799/koa-github-action
-docker container stop koa-docker
-docker container rm koa-docker
-docker run -d -p 8888:3010 --name $containerName smallzip7799/koa-github-action
+# 运行镜像容器
+docker run -d -p 8888:3010 --name $containerName "${imageName}"
