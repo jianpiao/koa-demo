900c075f8fc4e751a1a5fddb3b140814b0e9a7b6
zhoujianpiao <zhoujianpiao@ghzhushou.com>
2021-12-21 12:44:41 +0800

    命令

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -4,21 +4,21 @@ containerName="koa-docker"
 imageName="smallzip7799/koa-github-action"
 
 # 拉取镜像
-docker pull "${imageName}"
+docker pull smallzip7799/koa-github-action
 
 # 根据容器名称，检查容器是否存在
-docker inspect -f '{{.Name}}' $containerName
+docker inspect -f '{{.Name}}' koa-docker
 if [ $? == 0 ] ;then
     echo "docker exist"
     # 容器存在则先停掉
-    docker container stop $containerName
+    docker container stop koa-docker
     # 移除容器
-    docker container rm $containerName
+    docker container rm koa-docker
     # 移除镜像
-    docker images remove "${imageName}"
+    docker images remove smallzip7799/koa-github-action
 else
     echo "docker don't exist"
 fi
 
 # 运行镜像容器
-docker run -d -p 8888:3010 --name $containerName "${imageName}"
+docker run -d -p 8888:3010 --name koa-docker smallzip7799/koa-github-action
